afe43b6d4520c826a80617e4b2081e27baad5228
SaintEfim <saintefim2@gmail.com>
2024-02-12 13:29:52 +0300

    added new Entity

---
+++ microservices/Service.Authify/src/Service.Authify.Domain.Abstractions/Models/UserCredential.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Authify.Domain.Abstractions.Models;
+
+public class UserCredential
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = String.Empty;
+    public string Password { get; set; } = String.Empty;
+    public string Role { get; set; } = String.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
