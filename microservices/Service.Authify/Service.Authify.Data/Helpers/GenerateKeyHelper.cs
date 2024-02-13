891ef54042c542d5b4d60d6ae5bff82778b5906a
SaintEfim <saintefim2@gmail.com>
2024-02-13 15:09:38 +0300

    added GenerateKeyHelper

---
+++ microservices/Service.Authify/Service.Authify.Data/Helpers/GenerateKeyHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace Service.Authify.Data.Helpers;
+
+internal class GenerateKeyHelper
+{
+    public byte[] GenerateKey(string secretKey)
+    {
+        return Encoding.ASCII.GetBytes(secretKey);
+    }
+}
