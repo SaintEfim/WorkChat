f067a2095b17d86b0a4dd554b9f5de4ff8223995
SaintEfim <saintefim2@gmail.com>
2024-02-20 16:39:35 +0300

    added Dto models

---
+++ microservices/Service.Authify/src/Service.Authify.API/Models/ResponsesDto/LoginResponseDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Service.Authify.API.Models.ResponsesDto;
+
+/// <summary>
+/// The person responsible for some object processing or the one who is currently executing some task.
+/// </summary>
+public class LoginResponseDto
+{
+    /// <summary>
+    /// The type of token.
+    /// </summary>
+    [Required(ErrorMessage = "TokenType is required.")]
+    public string TokenType { get; set; } = String.Empty;
+
+    /// <summary>
+    /// The access token.
+    /// </summary>
+    [Required(ErrorMessage = "AccessToken is required.")]
+    public string AccessToken { get; set; } = String.Empty;
+
+    /// <summary>
+    /// The expiration time of the access token in seconds.
+    /// </summary>
+    [Required(ErrorMessage = "ExpiresIn is required.")]
+    public int ExpiresIn { get; set; }
+
+    /// <summary>
+    /// The refresh token.
+    /// </summary>
+    [Required(ErrorMessage = "RefreshToken is required.")]
+    public string RefreshToken { get; set; } = String.Empty;
+}
--- microservices/Service.Authify/src/Service.Authify.Domain.Abstractions/Models/Requests/RegistrationRequest.cs
+++ microservices/Service.Authify/src/Service.Authify.Domain.Abstractions/Models/Requests/RegistrationRequest.cs
@@ -4,5 +4,5 @@ public class RegistrationRequest
 {
     public string Email { get; set; } = String.Empty;
     public string Password { get; set; } = String.Empty;
-    public string Role { get; set; } = String.Empty;
+    public string? Role { get; set; }
 }
