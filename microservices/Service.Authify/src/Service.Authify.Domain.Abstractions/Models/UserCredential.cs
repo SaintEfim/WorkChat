9e1f0579ff07fdc0befe7c40ad06ad668fe992d1
SaintEfim <saintefim2@gmail.com>
2024-02-27 09:13:53 +0300

    remove fields

--- microservices/Service.Authify/src/Service.Authify.Domain.Abstractions/Models/UserCredential.cs
+++ microservices/Service.Authify/src/Service.Authify.Domain.Abstractions/Models/UserCredential.cs
@@ -2,10 +2,6 @@
 
 public class UserCredential
 {
-    public UserCredential()
-    {
-        Role = "user";
-    }
     public Guid Id { get; set; }
     public string Email { get; set; } = String.Empty;
     public string Password { get; set; } = String.Empty;
