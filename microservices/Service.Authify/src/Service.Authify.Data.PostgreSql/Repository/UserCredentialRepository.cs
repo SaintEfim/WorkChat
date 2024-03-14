d6cda857a529d41ea8971defd93cdc68e576b0be
SaintEfim <saintefim2@gmail.com>
2024-03-14 18:16:34 +0300

    reformat code

--- microservices/Service.Authify/src/Service.Authify.Data.PostgreSql/Repository/UserCredentialRepository.cs
+++ microservices/Service.Authify/src/Service.Authify.Data.PostgreSql/Repository/UserCredentialRepository.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Extensions.Logging;
 using Service.Authify.Data.PostgreSql.Context;
 using Service.Authify.Data.Repository;
-using Service.Authify.Domain.Models;
 
 namespace Service.Authify.Data.PostgreSql.Repository;
 
 public class UserCredentialRepository : UserCredentialRepository<UserCredentialDbContext>
 {
-    public UserCredentialRepository(UserCredentialDbContext context, ILogger<UserCredentialRepository> logger) : base(context, logger)
+    public UserCredentialRepository(UserCredentialDbContext context, ILogger<UserCredentialRepository> logger) : base(
+        context, logger)
     {
     }
 }
