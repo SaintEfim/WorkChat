b076ebcc51c1b07f55745755ebd7015d6ff9360a
SaintEfim <saintefim2@gmail.com>
2024-03-22 12:13:11 +0300

    warnings fix

--- microservices/Service.Authify/src/Service.Authify.API/Models/ErrorDto.cs
+++ microservices/Service.Authify/src/Service.Authify.API/Models/ErrorDto.cs
@@ -5,14 +5,14 @@ namespace Service.Authify.API.Models;
 public class ErrorDto
 {
     [JsonPropertyName("type")]
-    public string Type { get; set; }
+    public string Type { get; set; } = string.Empty;
 
     [JsonPropertyName("title")]
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
 
     [JsonPropertyName("status")]
     public int Status { get; set; }
 
     [JsonPropertyName("errors")]
-    public Dictionary<string, string[]> Errors { get; set; }
+    public Dictionary<string, string[]> Errors { get; set; } = null!;
 }
