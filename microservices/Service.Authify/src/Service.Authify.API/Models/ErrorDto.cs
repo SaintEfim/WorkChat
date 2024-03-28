42a94d0819939f13582f62061b76ec94f3ad9110
SaintEfim <saintefim2@gmail.com>
2024-03-28 12:14:59 +0300

    added summary comments

--- microservices/Service.Authify/src/Service.Authify.API/Models/ErrorDto.cs
+++ microservices/Service.Authify/src/Service.Authify.API/Models/ErrorDto.cs
@@ -2,17 +2,32 @@
 
 namespace Service.Authify.API.Models;
 
+/// <summary>
+/// Represents an error response DTO with details about the error.
+/// </summary>
 public class ErrorDto
 {
+    /// <summary>
+    /// The type of the error.
+    /// </summary>
     [JsonPropertyName("type")]
     public string Type { get; set; } = string.Empty;
 
+    /// <summary>
+    /// The title or summary of the error.
+    /// </summary>
     [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
 
-    [JsonPropertyName("status")] 
+    /// <summary>
+    /// The HTTP status code associated with the error.
+    /// </summary>
+    [JsonPropertyName("status")]
     public int Status { get; set; } = 500;
 
+    /// <summary>
+    /// Dictionary of field names and corresponding error messages.
+    /// </summary>
     [JsonPropertyName("errors")]
     public Dictionary<string, string[]> Errors { get; set; } = new();
 }
